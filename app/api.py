17ab58c71a9951311b895de50e030a83dda94867
Brandon Harrison <brandonharrisoncode@gmail.com>
2020-10-25 16:20:38 -0600

    Run through linter

--- app/api.py
+++ app/api.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify, make_response, request
-from flask_cors import CORS
+"""A GroupMe bot to autogenerate bingo cards"""
 import random
-import sys
 import os
 import requests
+
+from flask import Flask, request
+from flask_cors import CORS
 from selenium import webdriver
-from PIL import Image
-from io import BytesIO
 
 api = Flask(__name__)
 CORS(api)
@@ -24,11 +23,13 @@ options.add_argument("--no-sandbox")
 
 @api.route("/", methods=["GET"])
 def index():
+    """The debug method for the root path"""
     return "This is the Bingo Bot, how may I help you?"
 
 
 @api.route("/", methods=["POST"])
 def groupme_callback():
+    """The entry point for GroupMe callbacks"""
     data = request.get_json()
     if data.get("text", "") == CALL_PHRASE:
         return generate_bingo_card()
@@ -37,6 +38,7 @@ def groupme_callback():
 
 @api.route("/generate")
 def generate_bingo_card():
+    """Generates a unique bingo card from scratch, and uploads it to GroupMe"""
     with webdriver.Chrome(
         executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options
     ) as driver:
@@ -46,14 +48,13 @@ def generate_bingo_card():
     try:
         image_url = upload_image_to_groupme(screenshot_data)
         post_image_to_chat(image_url)
-    except RuntimeError as e:
-        if hasattr(e, "message"):
-            return str(e.message)
-        return str(e)
+    except RuntimeError as exception:
+        return str(exception)
     return "Success!"
 
 
 def upload_image_to_groupme(screenshot_data):
+    """Uploads the image of the bingo card (in binary data) to GroupMe's image servers"""
     upload_url = "https://image.groupme.com/pictures"
     headers = {
         "X-Access-Token": API_KEY,
@@ -68,6 +69,7 @@ def upload_image_to_groupme(screenshot_data):
 
 
 def post_image_to_chat(image_url):
+    """Sends a message to the group chat with the bingo card"""
     chat_url = "https://api.groupme.com/v3/bots/post"
     data = {
         "bot_id": BOT_ID,
@@ -83,6 +85,7 @@ def post_image_to_chat(image_url):
 
 
 def generate_html():
+    """Creates the bingo card in HTML"""
     html = start_html_doc()
     terms = get_bingo_terms()
     random.shuffle(terms)
@@ -93,6 +96,7 @@ def generate_html():
 
 
 def get_bingo_terms():
+    """Parses the text for the bingo boxes from the environment"""
     split_string = ";;;"
     if "BINGO_TEXT" in os.environ:
         terms = os.environ["BINGO_TEXT"].split(split_string)
@@ -104,6 +108,7 @@ def get_bingo_terms():
 
 
 def start_html_doc():
+    """Sets the preamble for the HTML to define styles"""
     head = """
     <!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">
             <html lang=\"en\">\n<head>
@@ -120,10 +125,10 @@ def start_html_doc():
 
 
 def generate_table(terms):
-    "Generates an HTML table representation of the bingo card for terms"
-    ts = terms[:12] + ["Logan is playing The Show (FREE SPACE)"] + terms[12:24]
+    """Generates an HTML table representation of the bingo card for terms"""
+    box_contents = terms[:12] + ["Logan is playing The Show (FREE SPACE)"] + terms[12:24]
     res = "<table id='bingocard'>\n"
-    for i, term in enumerate(ts):
+    for i, term in enumerate(box_contents):
         if i % 5 == 0:
             res += "\t<tr>\n"
         res += "\t\t<td>" + term + "</td>\n"
